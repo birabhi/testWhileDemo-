945cc90fdc9d95f255fd4f43377219c5add4e15d
birabhi <jamkhandi177@gmail.com>
2022-05-23 16:02:26 +0530

    First Commit

---
+++ WhileDemoCode/src/WhileDemoCode.java
@@ -0,0 +1,15 @@
+
+public class WhileDemoCode {
+
+	public static void main(String[] args[]) {
+		int i = 5;
+		while (i<=15) {
+			System.out.println(i);
+			i+2;
+		}
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
